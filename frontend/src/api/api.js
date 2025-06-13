9f2d4d9b64507bbffb5c7e32524375e874ea32c5
Ricardo Triques <113929141+ricardotriquers@users.noreply.github.com>
2025-06-13 16:46:59 -0300

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api',
+  baseURL: 'http://localhost:8080/api' // Spring Boot rodando localmente
 });
 
 // Add request interceptor to include token
